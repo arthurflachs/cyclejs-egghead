87221f4cdfa5d2efa1452c443b02feab1c5f49ca
Arthur Flachs <arthur.flachs@gmail.com>
2016-01-29 21:52:13 +0100

    Returns multiple event streams

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const Rx = require('rx')
 
 // Logic: Event stream (functional)
 function main() {
-  return Rx.Observable.timer(0, 1000)
-    .map(i => `Seconds elapsed ${i}`)
+  return {
+    DOM: Rx.Observable.timer(0, 1000)
+      .map(i => `Seconds elapsed ${i}`),
+    Log: Rx.Observable.timer(0, 2000).map(i => 2 * i),
+  }
 }
 
 function DOMEffect(text$) {
@@ -18,6 +21,6 @@ function consoleLogEffect(msg$) {
   msg$.subscribe(msg => console.log(msg))
 }
 
-const sink = main()
-DOMEffect(sink)
-consoleLogEffect(sink)
+const sinks = main()
+DOMEffect(sinks.DOM)
+consoleLogEffect(sinks.Log)
