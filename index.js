f11bb5a32781d42d914467dddc5c9b7530df7800
Arthur Flachs <arthur.flachs@gmail.com>
2016-01-29 22:42:34 +0100

    Change DOM driver to create actual DOM elements from object)

--- index.js
+++ index.js
@@ -9,7 +9,19 @@ function main(sources) {
       .startWith(null)
       .flatMapLatest(() =>
         Rx.Observable.timer(0, 1000)
-          .map(i => `Seconds elapsed ${i}`)
+        .map(i => {
+          return {
+            tagName: 'H1',
+            children: [
+              {
+                tagName: 'SPAN',
+                children: [
+                  `Seconds elapsed ${i}`
+                ]
+              },
+            ]
+          }
+        })
       ),
     Log: Rx.Observable.timer(0, 2000).map(i => 2 * i),
   }
@@ -17,10 +29,28 @@ function main(sources) {
 }
 
 // Drivers: change the external world (imperative)
-function DOMDriver(text$) {
-  text$.subscribe(text => {
+function DOMDriver(obj$) {
+  function createElement(obj) {
+    const element = document.createElement(obj.tagName)
+
+    const child = obj.children
+      .forEach(c => {
+        if (typeof c === 'object') {
+          element.appendChild(createElement(c))
+          return c;
+        }
+        element.innerHTML += c
+        return c
+      })
+
+    return element
+  }
+
+  obj$.subscribe(obj => {
     const container = document.querySelector('#app')
-    container.textContent = text
+    container.innerHTML = ''
+    const element = createElement(obj)
+    container.appendChild(element)
   })
 
   const DOMSource = Rx.Observable.fromEvent(document, 'click')
