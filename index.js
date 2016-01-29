080d22860eb1ab03545511abf0580ba3a6d3dc9f
Arthur Flachs <arthur.flachs@gmail.com>
2016-01-29 23:35:58 +0100

    Counter example

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 import Rx from 'rx'
 import Cycle from '@cycle/core'
-import { label, input, h1, hr, div, makeDOMDriver } from '@cycle/dom'
+import { button, p, label, div, makeDOMDriver } from '@cycle/dom'
 
 // Logic: Event stream (functional)
 function main(sources) {
-  const inputEv$ = sources.DOM.select('.field').events('input')
-  const name$ = inputEv$.map(ev => ev.target.value).startWith('')
+  const decrementClick$ = sources.DOM
+    .select('.decrement').events('click')
+  const incrementClick$ = sources.DOM
+    .select('.increment').events('click')
+  const decrementAction$ = decrementClick$.map(ev => -1)
+  const incrementAction$ = incrementClick$.map(ev => +1)
+
+  const number$ = Rx.Observable.of(0)
+    .merge(decrementAction$).merge(incrementAction$)
+    .scan((prev, cur) => prev + cur)
 
   return {
-    DOM: name$.map(name =>
+    DOM: number$.map(number =>
       div([
-        label('Name:'),
-        input('.field', {type: 'text'}),
-        hr(),
-        h1(`Hello ${name} !`)
+        button('.decrement', 'Decrement'),
+        button('.increment', 'Increment'),
+        p([
+          label(String(number))
+        ])
       ])
     )
   }
