693a1b63946ff4802b12d6c44578973bdaa21b6d
Arthur Flachs <arthur.flachs@gmail.com>
2016-01-29 23:16:44 +0100

    Resets application

--- index.js
+++ index.js
@@ -7,31 +7,11 @@ const makeDOMDriver = CycleDOM.makeDOMDriver
 
 // Logic: Event stream (functional)
 function main(sources) {
-  const mouseover$ = sources.DOM.select('.foo').events('mouseover')
-
-  const sinks = {
-    DOM: mouseover$
-      .startWith(null)
-      .flatMapLatest(() =>
-        Rx.Observable.timer(0, 1000)
-        .map(i => h1({style: {background: 'red'}}, [
-          span([
-            `Seconds elapsed ${i}`
-          ])
-        ]))
-      ),
-    Log: Rx.Observable.timer(0, 2000).map(i => 2 * i),
-  }
-  return sinks
-}
-
-function consoleLogDriver(msg$) {
-  msg$.subscribe(msg => console.log(msg))
+  return  {}
 }
 
 const drivers = {
   DOM: makeDOMDriver('#app'),
-  Log: consoleLogDriver,
 }
 
 Cycle.run(main, drivers)
