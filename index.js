5ec699bf52c95392438ad76770569710dbea57d9
Arthur Flachs <arthur.flachs@gmail.com>
2016-01-29 23:14:11 +0100

    Uses cycle/dom

--- index.js
+++ index.js
@@ -1,26 +1,20 @@
-function h(tagName, children) {
-  return {
-    tagName: tagName,
-    children: children,
-  }
-}
-
-const h1 = h.bind(null, 'H1')
-const span = h.bind(null, 'SPAN')
-
 const Rx = require('rx')
 const Cycle = require('@cycle/core')
+const CycleDOM = require('@cycle/dom')
+const h1 = CycleDOM.h1
+const span = CycleDOM.span
+const makeDOMDriver = CycleDOM.makeDOMDriver
 
 // Logic: Event stream (functional)
 function main(sources) {
-  const mouseover$ = sources.DOM.selectEvents('span', 'mouseover')
+  const mouseover$ = sources.DOM.select('.foo').events('mouseover')
 
   const sinks = {
     DOM: mouseover$
       .startWith(null)
       .flatMapLatest(() =>
         Rx.Observable.timer(0, 1000)
-        .map(i => h1([
+        .map(i => h1({style: {background: 'red'}}, [
           span([
             `Seconds elapsed ${i}`
           ])
@@ -31,46 +25,12 @@ function main(sources) {
   return sinks
 }
 
-// Drivers: change the external world (imperative)
-function DOMDriver(obj$) {
-  function createElement(obj) {
-    const element = document.createElement(obj.tagName)
-
-    const child = obj.children
-      .forEach(c => {
-        if (typeof c === 'object') {
-          element.appendChild(createElement(c))
-          return c;
-        }
-        element.innerHTML += c
-        return c
-      })
-
-    return element
-  }
-
-  obj$.subscribe(obj => {
-    const container = document.querySelector('#app')
-    container.innerHTML = ''
-    const element = createElement(obj)
-    container.appendChild(element)
-  })
-
-  const DOMSource = {
-    selectEvents: function(tagName, eventType) {
-      return Rx.Observable.fromEvent(document, eventType)
-        .filter(ev => ev.target.tagName === tagName.toUpperCase())
-    }
-  }
-  return DOMSource
-}
-
 function consoleLogDriver(msg$) {
   msg$.subscribe(msg => console.log(msg))
 }
 
 const drivers = {
-  DOM: DOMDriver,
+  DOM: makeDOMDriver('#app'),
   Log: consoleLogDriver,
 }
 
