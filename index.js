984625e6cb475757c5226292df0a23ccb381265c
Arthur Flachs <arthur.flachs@gmail.com>
2016-01-29 21:49:04 +0100

    Separates logic, allows another effect to suscribe to the event stream

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const Rx = require('rx')
 
 // Logic: Event stream (functional)
-Rx.Observable.timer(0, 1000)
-  .map(i => `Seconds elapsed ${i}`)
+function main() {
+  return Rx.Observable.timer(0, 1000)
+    .map(i => `Seconds elapsed ${i}`)
+}
 
+function DOMEffect(text$) {
 // Effects: change the external world (imperative)
-  .subscribe(text => {
+  text$.subscribe(text => {
     const container = document.querySelector('#app')
     container.textContent = text
   })
+}
+
+function consoleLogEffect(msg$) {
+  msg$.subscribe(msg => console.log(msg))
+}
+
+const sink = main()
+DOMEffect(sink)
+consoleLogEffect(sink)
